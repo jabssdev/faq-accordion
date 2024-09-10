64e6b702d5d451d9c56cf3888329d94eb8dd5fb5
JabssDev <jordanburgoss23@gmail.com>
2024-09-09 20:54:35 -0500

    chore: :art: Add customizable fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"light-pink": "hsl(275, 100%, 97%)",
+				"grayish-purple": "hsl(292, 16%, 49%)",
+				"dark-purple": "hsl(292, 42%, 14%)",
+			},
+			fontFamily: {
+				sans: ["Work sans", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
